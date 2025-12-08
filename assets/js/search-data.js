c5da44d75f3ee3904b5bbf13d5a84a599701bd8b
gaurav-rattan <g.rattan@utwente.nl>
2025-12-08 13:03:37 +0000

    Deploying to gh-pages from @ gaurav-rattan/gaurav-rattan.github.io@8b0671b28e24f04d44f3bfe895a10240b2fd397a 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -14,7 +14,7 @@ ninja.data = [{
           description: "",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/blog/";
+            window.location.href = "/LOGSMS/index.html";
           },
         },{id: "nav-news",
           title: "news",
